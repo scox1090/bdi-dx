dd193b7e02eada3e351486777b302b6b35c3c8fa
KOITT <KOITT@KOITT-PC>
2018-10-17 18:05:57 +0900

    dasda

--- src/main/java/com/bdi/sp/vo/Us.java
+++ src/main/java/com/bdi/sp/vo/Us.java
@@ -3,3 +3,18 @@ package com.bdi.sp.vo;
 public class Us {
 
 }
+/*create table user(
+usno INTEGER PRIMARY KEY AUTO_INCREMENT,
+usid varchar(200) not null, 
+uspwd varchar(200) not null,
+usname varchar(200) not null,
+usemail varchar(300) not null,
+usaddress varchar(300) not null,
+ustel varchar(200) not null,
+usgender varchar(100) not null, /성별
+ushobby varchar(200)  null, /취미
+usrecommender varchar(200)  null /추천인
+);
+​
+insert into user(usid,uspwd,usname,usemail,usaddress,ustel,usgender,ushobby,usrecommender)
+values('test','test','test','test','test','test','test','test','test');*/​
